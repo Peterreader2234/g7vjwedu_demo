0490962bd842fc5be05de262a4245e0d36bb0eff
Peterreader2234 <86353079+Peterreader2234@users.noreply.github.com>
2021-06-23 21:10:29 -0700

    Update Header.php

--- Header.php
+++ Header.php
@@ -1,9 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+.topnav-left {
+  float: left;
+}
+</style>
+</head>
+<body>
 <div id="title">
-    <ul id="navbar">
+	<ul id="navbar">
+		<div class="topnav-left">
          <li><a href="/">
-            <img src="//static.foxnews.com/static/orion/styles/img/fox-news/bg/fn-logo-watch-now.png" class="responsive-img" style="max-height:80px"/>
+            <img src="//static.foxnews.com/static/orion/styles/img/fox-news/bg/fn-logo-watch-now.png" class="responsive-img" style="max-height:80px" height="57" width="57" />
             </a>
          </li>
+		</div>
      </ul>
     <h1><span style="color: #008000;">ONLINE CLOTHING STORE</span></h1>
   </div>
@@ -19,3 +61,5 @@
       <li><a href="Contact.php">Contact</a></li>
     </ul>
   </div>
+</body>
+</html>
